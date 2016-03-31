39c5ec1c99ff09f4a4799d2a7f26d58f43b65aa3
Zach Smith <Zacitus@Zacharys-MacBook-Pro.local>
2016-03-31 05:23:05 -0500

    Tiles Now Know What Tiles Are Near Them

--- Assets/ObjectManager.cs
+++ Assets/ObjectManager.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class ObjectManager : MonoBehaviour {
 
-	static public int rows = 11;
-	static public int columns = 11;
+	static public int width = 11;
+	static public int height = 11;
 
 	public GameObject tilePrefab;
 
@@ -13,22 +13,27 @@ public class ObjectManager : MonoBehaviour {
 	public Material notForSale;
 	public Material actionTrigger;
 
-	public TileProperties tileprops;
-
-	public GameObject[,] tileArray = new GameObject[rows, columns];
+	public GameObject[,] tileArray = new GameObject[width, height];
 
 
 
 	void Awake ()
 	{
-	int i = 1;
 
-		for (int x = 0; x < columns; x++)
+		int i = 0;
+
+		for (int y = 0; y < height; y++)
 		{
-			for (int y = 0; y < rows; y++)
+			for (int x = 0; x < width; x++)
 			{
-				tileArray[x,y] = (Instantiate(tilePrefab, new Vector3(y, 0, -x), Quaternion.identity)) as GameObject;
-				tileArray[x,y].GetComponent<TileProperties>().ID = i;
+				tileArray [x, y] = (Instantiate (tilePrefab, new Vector3 (x, 0, y), Quaternion.identity)) as GameObject;
+				tileArray [x, y].GetComponent<TileProperties> ().ID = i;
+
+				if (i > 77)
+				{
+					tileArray [x, y].GetComponent<Renderer> ().material = notForSale;
+				}
+
 				i++;
 			}
 
@@ -39,25 +44,26 @@ public class ObjectManager : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
 	{
-
+	/*
 		// Divide the boy's and girl's restroom by dividing the map in half
-		for (int i = 0; i < rows / 2; i++)
+		for (int i = 0; i < width / 2; i++)
 		{
-			tileArray [rows/2, i].gameObject.GetComponent<Renderer> ().material = notForSale;
+			tileArray [width/2, i].gameObject.GetComponent<Renderer> ().material = notForSale;
 		}
-
+		*/
 	}
 	// Update is called once per frame
 	void Update () {
-
+	/*
 		// Make part of the map not for sale (yet)
-		for (int x = columns - 1; x > -1; x--)
+		for (int x = height - 1; x > -1; x--)
 		{
-			for (int y = rows - 1; y > 4; y--)
+			for (int y = width - 1; y > 4; y--)
 			{
 				tileArray [x, y].gameObject.GetComponent<Renderer> ().material = notForSale;
 			}
 		}
-
+		*/
 	}
+
 }
--- Assets/Scripts/TileProperties.cs
+++ Assets/Scripts/TileProperties.cs
@@ -12,19 +12,84 @@ public class TileProperties : MonoBehaviour {
 	public GameObject tileLeft;
 	public GameObject tileRight;
 
+	public int tileUpperInt;
+	public int tileLowerInt;
+	public int tileLeftInt;
+	public int tileRightInt;
+
 	public GameObject tileUpperRight;
 	public GameObject tileUpperLeft;
 	public GameObject tileLowerRight;
 	public GameObject tileLowerLeft;
 
+	public int tileUpperRightInt;
+	public int tileUpperLeftInt;
+	public int tileLowerRightInt;
+	public int tileLowerLeftInt;
+
+	private GameObject ObjectManagerGameObject;
+
+	public GameObject tempGameObject;
+
 	// Use this for initialization
 	void Start () {
 
+		ObjectManagerGameObject = GameObject.Find("ObjectManager");
 
+		int arrayWidth = ObjectManager.width;
+		//Debug.Log("arrayWidth is " + arrayWidth);
+		int arrayHeight = ObjectManager.height;
+		//Debug.Log("arrayHeight is " + arrayHeight);
+
+		tileUpperInt = ID + arrayWidth;
+		tileLowerInt = ID - arrayWidth;
+		tileLeftInt = ID - 1;
+		tileRightInt = ID + 1;
+
+		tileUpperRightInt = ID + arrayWidth + 1;
+		tileUpperLeftInt = ID + arrayWidth - 1;
+		tileLowerRightInt = ID - arrayWidth + 1;
+		tileLowerLeftInt = ID - arrayWidth - 1;
+
+		tileUpper = AssignTile(tileUpperInt);
+		tileLower = AssignTile(tileLowerInt);
+		tileLeft = AssignTile(tileLeftInt);
+		tileRight = AssignTile(tileRightInt);
+		tileUpperRight = AssignTile(tileUpperRightInt);
+		tileUpperLeft = AssignTile(tileUpperLeftInt);
+		tileLowerRight = AssignTile(tileLowerRightInt);
+		tileLowerLeft = AssignTile(tileLowerLeftInt);
 	}
-	
+
+	GameObject AssignTile (int tileNumber)
+
+	{
+		int arrayMax = ObjectManager.width * ObjectManager.height;
+
+		if (tileNumber > arrayMax - 1 || tileNumber < 0)
+		{
+			return null;
+		} 
+		else if (tileNumber < ObjectManager.width - 1)
+		{
+			Debug.Log("Else if triggered!");
+
+			return(ObjectManagerGameObject.GetComponent<ObjectManager> ().tileArray [(tileNumber), 0]);
+
+		} 
+		else if (tileNumber > ObjectManager.width - 1) {
+			int remainder = tileNumber % ObjectManager.width;
+
+			return(ObjectManagerGameObject.GetComponent<ObjectManager> ().tileArray [remainder, tileNumber/ObjectManager.width]);
+		}
+		else
+		{
+			return null;
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
